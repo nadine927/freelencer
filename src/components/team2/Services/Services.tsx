4bcdbe0057b9fe706c57c42bfb6d0ba90354cef7
Gasana Theophile <gasana141414@gmail.com>
2022-01-11 15:57:55 +0200

    changes

--- src/components/team2/Services/Services.tsx
+++ src/components/team2/Services/Services.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
@@ -8,7 +9,228 @@ export default function Services (){
     return(
         <>
         <div className="h-screen w-full">
-        <img src={BackgroundImage} className="w-full h-screen" />
+        <img src={BackgroundImage} className="w-full h-screen absolute" />
+        <div className="h-20 w-1/2 relative">
+            <div className="flex justify-evenly h-20 w-full">
+            <a className="pt-14 text-white text-xl" href="" >Home</a>
+            <a className="pt-14 text-white text-xl" href="">About</a>
+            <a className="pt-14 text-amber-300 font-bold text-xl" href="">Services</a>
+            <a className="pt-14 text-white text-xl" href="">Support</a>
+            <a className="pt-14 text-amber-300 font-bold text-xl" href="">Join our Community</a>
+            </div>
+        </div>
+        <div className="h-20 w-1/2  flex relative mt-6">
+            <div className="h-10 w-1/2 ml-48 mt-6 bg-green-400">
+            <input type="text" className="h-10 w-full"  />
+            </div>
+            <div className="h-10 w-1/4 bg-amber-300 mt-6">
+                <button className="btn text-xl ml-16 mt-1  text-white">Search</button>
+            </div>
+        </div>
+        <div className="h-40 w-1/2 relative">
+        <div className="flex justify-evenly mt-1 ">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+         <div className="flex justify-evenly mt-1">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+         <div className="flex justify-evenly mt-1 ">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+        </div>
+        <div className="h-40 w-1/2 relative">
+        <div className="flex justify-evenly mt-1">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+         <div className="flex justify-evenly mt-1">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+         <div className="flex justify-evenly mt-1">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+        </div>
+        <div className="h-40 w-1/2 relative">
+        <div className="flex justify-evenly mt-1">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+         <div className="flex justify-evenly mt-1">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+         <div className="flex justify-evenly mt-1">
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+             <a href="">
+            <h5 className="text-white text-center mt-1">Web Based App</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44  border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Mobile App Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+            <a href="">
+            <h5 className="text-white text-center mt-1">Ussd Dev</h5>
+            </a>
+         </div>
+         <div className="h-10 w-44 border-solid border-2 border-amber-300 ml-11 mt-2">
+         <a href="">
+            <h5 className="text-white text-center mt-1">UI/UX</h5>
+            </a>
+         </div>
+         </div>
+        </div>
         </div>
         </>
     )
