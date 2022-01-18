54105b1cc398d81e479fc88fb7e93dfe383bcc3c
Gasana Theophile <gasana141414@gmail.com>
2022-01-18 20:19:14 +0200

    changes

--- src/components/pages/Services.tsx
+++ src/components/pages/Services.tsx
@@ -33,7 +33,7 @@ export default function Services (){
         <div className='h-96 md:relative md:bg-black bg-mobile md:w-1/2 md:mt-28 mt-0 md:ml-20'>
             <div className='flex mt-0 md:mt-2 mr-2 md:mr-0 ml-2 md:ml-0'>
             <div className='h-10 w-2/3 border-2 border-gold'>
-                <h5 className='text-center text-white pt-1 text-sm md:text-base font-bold'>Web Based App Dev</h5>
+                <h5 className='text-center text-white pt-1 text-sm md:text-base font-bold'>Web App Dev</h5>
             </div>
             <div className='h-10 w-2/3 border-2 border-gold ml-2'>
             <h5 className='text-center text-white pt-1 text-sm md:text-base font-bold'>Mobile App Dev</h5>
