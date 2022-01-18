0d6303ad6c693d514b21b1dc212fc820e4cb2443
Gasana Theophile <gasana141414@gmail.com>
2022-01-18 14:02:36 +0200

    Home Page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -8,18 +8,15 @@ export default function Home(){
 
     return(
         <>
-        <div className="h-screen w-full ">
+        <div className="h-screen w-full bg-mobile ">
         <Background/>
         <Navbar/>
 
-        <div className='relative pt-40 h-screen md:px-12  bg-mobile' >
+        <div className='relative md:mt-80 md:h-60 mt-56 md:px-12 md:w-1/2' >
         <p className='text-white font-bold text-2xl md:text-3xl ml-3'><span className='text-gold px-2'>WE ARE</span><span className='text-gold md:text-white'>NIGHT </span> <span>DEVELOPERS</span></p>
         <p className='text-white text-lg mt-2 px-2 ml-3'> Hire affordable professinal individual or a team of freelancers</p>
         <p className='text-white text-lg px-2 ml-3'>We are here to faster your work, and deadlines to help your stay the best</p>
-        <div className="grid md:grid-cols-2 ml-3 ">
 
-
-   </div>
    <div className='h-14 w-full mt-2  flex '>
        <div className='h-14 w-3/6 border-2 border-gold ml-4'>
             <h4 className='text-center text-white text-xl font-bold pt-2'>Hire a pro freelancer</h4>
--- src/components/pages/Navbar.tsx
+++ src/components/pages/Navbar.tsx
@@ -8,12 +8,12 @@ export default function Navbar(){
         console.log(toggle)
     }
     return(<>
-        <div onClick={toggleMe} className="md:hidden bg-mobile h-14 w-full " >
+        <div onMouseOver={toggleMe} className="md:hidden bg-mobile h-14  md:h-0 w-full  md:w-0" >
                 <  FaBars size={45} style={{color:'white'}} className='ml-3 pt-3' />
         </div>
-       <div className="md:h-20 w-full relative bg-mobile">
+       <div className="h-0 md:h-0 w-full md:w-1/2 relative bg-mobile">
             <div className="hidden md:block">
-                <div className="flex justify-around h-20 w-full ml-20">
+                <div className="flex justify-between h-20 w-full ml-20 md:ml-14">
                 <a className="pt-14 text-gold font-bold text-2xl font-serif " href={FrontEndEnums.Home} >Home</a>
                 <a className="pt-14 text-white text-2xl font-serif" href={FrontEndEnums.About}>About</a>
                 <a className="pt-14 text-white  text-2xl font-serif" href={FrontEndEnums.Service}>Services</a>
@@ -23,12 +23,12 @@ export default function Navbar(){
             </div>
 
            {toggle?
-            <div className="md:ml-0 ml-6 pt-1 h-56">
-            <div className='h-10 w-5/6 border-t-2 border-b-2 border-gray-400'><a className="pt-14 md:text-gold text-white font-bold text-xl" href={FrontEndEnums.Home} >Home</a></div>
-            <div className='h-10 w-5/6 border-b-2 border-gray-400'><a className="pt-14 text-white text-xl" href={FrontEndEnums.About}>About</a></div>
-            <div className='h-10 w-5/6 border-b-2 border-gray-400'><a className="pt-14 text-white  text-xl" href={FrontEndEnums.Service}>Services</a></div>
-            <div className='h-10 w-5/6 border-b-2 border-gray-400'><a className="pt-14  text-white text-xl" href={FrontEndEnums.Support }>Support</a></div>
-            <div className='h-10 w-5/6 border-b-2 border-gray-400'><a className="pt-14 md:text-gold text-white text-xl" href={FrontEndEnums.JoinCommunity }>Join our Community</a></div>
+            <div className="md:ml-0 ml-0  md:h-0 h-56 bg-mobile">
+            <div className='h-10 ml-6 w-5/6 md:border-0 md:border-t-0 md:border-b-0 border-t-2 border-b-2 border-gray-400'><a className="pt-14 md:text-black text-white font-bold text-xl" href={FrontEndEnums.Home} >Home</a></div>
+            <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><a className="pt-14 text-white md:text-black text-xl" href={FrontEndEnums.About}>About</a></div>
+            <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><a className="pt-14 text-white md:text-black  text-xl" href={FrontEndEnums.Service}>Services</a></div>
+            <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><a className="pt-14  text-white md:text-black text-xl" href={FrontEndEnums.Support }>Support</a></div>
+            <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><a className="pt-14 md:text-black  text-white text-xl" href={FrontEndEnums.JoinCommunity }>Join our Community</a></div>
 
             </div>:
             <span></span>
