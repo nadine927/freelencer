cd4c634e9e93205796a3eec214df397502ce843a
Gasana Theophile <gasana141414@gmail.com>
2022-01-18 20:13:20 +0200

    changes

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -27,9 +27,9 @@ export default function About (){
              </div>
 
             <div className='text-2xl md:ml-16 ml-6 md:flex h-44 w-5/6 md:w-1/2 relative mt-2'>
-                <div className="md: mt-2 h-12 py-2 w-26 border-2 border-gold text-xl text-white px-8"><p><b>Web Based App Dev</b></p></div>
-                <div className="mt-2 md:ml-2 h-12 py-2 w-26 border-2 border-gold text-xl border-border-4 px-8 text-white"><p><b>Mobil App Dev</b></p></div>
-                <div className="mt-2 md:ml-2 h-12 py-2 w-26 border-2 border-gold text-xl border-border-4 px-8 text-white"><p><b>USSD Dev</b></p></div>
+                <div className="md: mt-2 h-12 w-26 border-2 border-gold text-white px-8"><h4 className='font-bold  text-base md:text-xl md:pt-1.5 pt-2'>Web Based App Dev</h4></div>
+                <div className="mt-2 md:ml-2 h-12 w-26 border-2 border-gold border-border-4 px-8 text-white"><h4 className='font-bold text-base md:text-xl md:pt-1.5 pt-2'>Mobile App Dev</h4></div>
+                <div className="mt-2 md:ml-2 h-12 w-26 border-2 border-gold border-border-4 px-8 text-white"><h4 className='font-bold text-base md:text-xl md:pt-1.5 pt-2'>Ussd Dev</h4></div>
 
             </div>
 
