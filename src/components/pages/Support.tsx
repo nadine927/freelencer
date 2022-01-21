9039cb5b5a88de5b1def39ceed383f7de437c78c
Patrick <kingpatrick250@gamil.com>
2022-01-21 08:55:17 +0200

    added

--- src/components/pages/Support.tsx
+++ src/components/pages/Support.tsx
@@ -13,7 +13,7 @@ export default function Support()
 
             <div className="flex bg-mobile">
             <div className="relative w-full mt-64 md:mt-64 mx-10 ">
-                <div className='w-full md:flex'>
+                <div className='w-full md:flex ml-7'>
                 <div className="border-2 mt-3 md:border-2 border-gold md:w-80 md:mr-3 ">
                     <span className="px-3 text-white font-bold">Rwanda-Kigali</span>
                     <span className="px-3 flex flex-row text-white mt-2">Kigali@freelancers.rw</span>
@@ -25,7 +25,7 @@ export default function Support()
                     <span className="px-3 flex flex-row text-white "><span className="text-gold px-1">(+250)</span>789 548 265</span>
                 </div>
                 </div>
-                <div className="w-full md:flex">
+                <div className="w-full md:flex ml-7">
                 <div className="border-2 mt-3 md:border-2 border-gold w-full md:w-80 md:mr-3">
                     <span className="px-3 text-white font-bold">Rdc-Goma</span>
                     <span className="px-3 flex flex-row text-white mt-2">Goma@freelancers.rw</span>
@@ -37,7 +37,7 @@ export default function Support()
                     <span className="px-3 flex flex-row text-white "><span className="text-gold px-1">(+250)</span>789 548 265</span>
                 </div>
                 </div>
-                <div className="w-full md:flex">
+                <div className="w-full md:flex ml-7">
                 <div className="border-2 mt-3 md:border-2 border-gold w-full md:w-80 md:mr-3">
                     <span className="px-3 text-white font-bold">Zimbabwe-Harare</span>
                     <span className="px-3 flex flex-row text-white mt-2">Harare@freelancers.rw</span>
